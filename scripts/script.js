70f29c7bd799b90270c244f0db52d7287ed7a4c3
ShevchenkoJ <shevchenko.juliia@gmail.com>
2019-08-19 00:15:23 +0300

    Add copy buttonfixed footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 	$('.owl-carousel').owlCarousel({
         items:1,
-        loop:true,
+		loop:true,
+		rewind:false,
         center:true,
-        nav: true,
-        margin:0,
+        nav:true
+        /*margin:0,
         URLhashListener:true,
         autoplayHoverPause:true,
-        startPosition: 'URLHash'
+        startPosition: 'URLHash'*/
     });
 
 	$('.owl-carousel').on('changed.owl.carousel', function(e) {
@@ -15,11 +16,22 @@ $(document).ready(function() {
   	$('.header__link').removeClass('active-link');
   	$('.header__link').eq(e.item.index - 3).addClass('active-link');  	
 	});
+
+	/*$('.header__link').eq(e.item.index - 3).addClass('active-link');  	
+	});*/
     
 	$('.header__link').click(function() {
 	    $('.header__link').removeClass('active-link');
 	    $(this).addClass('active-link');
 	});
+
+	/*if ( $('.slide-background').hasClass('slide-1') ) {
+		$('.list_item-1').toggleClass('active-link');
+	};
+
+	if ( $('.slide-background').hasClass('slide-2') ) {
+		$('.list_item-2').toggleClass('active-link');
+	};*/
 
 	$('.burger-menu_icon__wrapper').click(function() {
 	    $('.burger-menu_icon').toggleClass('burger-menu_icon__active');
@@ -38,5 +50,14 @@ $(document).ready(function() {
 		$('.owl-next').trigger('click');
 	}); 
 
+	$('.copy_button').click(function() {
+		var $temp = $("<input>");
+		$("body").append($temp);
+		$temp.val($('.copy_content').text()).select();
+		document.execCommand("copy");
+		alert("Content copied");
+		$temp.remove();
+	});
+	
 });
 
